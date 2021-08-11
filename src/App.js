2ea337253c48b6adbe4e59870f5da523473e169e
Grigorios Kaframanis <gkaframanis@gmail.com>
2021-08-11 18:28:02 +0300

    Using Stateful Lists and Creating conditional content

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+
+const DUMMY_EXPENSES = [
+	{ id: "e1", title: "Car Insurance", amount: 294.67, date: new Date(2021, 7, 14) },
+	{ id: "e2", title: "TV", amount: 799.49, date: new Date(2021, 2, 12) },
+	{ id: "e3", title: "Laptop", amount: 1000, date: new Date(2021, 2, 28) },
+	{ id: "e4", title: "Desk", amount: 450, date: new Date(2021, 5, 12) },
+];
+
 // Our root component
-function App(props) {
-	const expenses = [
-		{ id: "e1", title: "Car Insurance", amount: 294.67, date: new Date(2021, 7, 14) },
-		{ id: "e2", title: "TV", amount: 799.49, date: new Date(2021, 2, 12) },
-		{ id: "e3", title: "Laptop", amount: 1000, date: new Date(2021, 2, 28) },
-		{ id: "e4", title: "Desk", amount: 450, date: new Date(2021, 5, 12) },
-	];
+function App() {
+	const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
 	const addExpenseHandler = expense => {
-		console.log("In App.js");
-		console.log(expense);
+		setExpenses(prevExpenses => [expense, ...prevExpenses]);
 	};
 
 	// JSX Syntax: JavaScript XML
--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -13,16 +13,37 @@ const Expenses = props => {
         setFilteredYear(selectedYear);
     };
 
+	const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() === filteredYear)
+
+	// We can store JSX in variables.
+	// 3rd way
+	let expensesContent = <p>No expenses found</p>;
+
+	if(filteredExpenses.length > 0){
+		expensesContent = filteredExpenses.map(expense => <ExpenseItem key={expense.id} {...expense}/>);
+	}
+	
 	return (
 		<Card className="expenses">
-            {/* ExpensesFilter is a controlled component. It's controlled by the Expenses Component. */}
+			{/* ExpensesFilter is a controlled component. It's controlled by the Expenses Component. */}
 			<ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-			<ExpenseItem {...props.items[0]} />
-			<ExpenseItem {...props.items[1]} />
-			<ExpenseItem {...props.items[2]} />
-			<ExpenseItem {...props.items[3]} />
+			{/* We transform our array to an array of JSX elements */}
+			{/* Always use the key prop when you mapping out items. */}
+
+			{/* ------- Creating a conditional Content ----------*/}
+
+			{/* 1st Way */}
+			{/* {filteredExpenses.length === 0
+				? <p>No expenses found</p> 
+				: filteredExpenses.map(expense => <ExpenseItem key={expense.id} {...expense}/>)}	 */}
+			{/* If the first condition is true then the second is executed. */}
+
+			{/* 2nd Way */}
+			{/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+			{filteredExpenses.length > 0 && filteredExpenses.map(expense => <ExpenseItem key={expense.id} {...expense}/>)} */}
+			{expensesContent}
 		</Card>
-	);
+	)
 };
 
 export default Expenses;
